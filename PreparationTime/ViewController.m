59182ce88bbc1818d6ad1b7a99ca7b2c0fe89319
Doug Waters <dougwaters@wireless-10-146-128-244.public.utexas.edu>
2015-03-23 11:17:01 -0500

    First Commit

--- PreparationTime/ViewController.m
+++ PreparationTime/ViewController.m
@@ -9,19 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *wakeUp;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)wakeUpUser:(id)sender
+{
+    NSLog(@"User wants to wake up.");
 }
 
 @end
