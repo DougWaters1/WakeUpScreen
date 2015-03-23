c9ab2cfd8fc8cd19de537544964a75d1d9ca9cc6
Doug Waters <dougwaters@wireless-10-146-128-244.public.utexas.edu>
2015-03-23 10:32:50 -0500

    Initial Commit

---
+++ PreparationTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PreparationTime
+//
+//  Created by Doug Waters on 3/23/15.
+//  Copyright (c) 2015 iSleepLate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PreparationTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PreparationTime
+//
+//  Created by Doug Waters on 3/23/15.
+//  Copyright (c) 2015 iSleepLate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
